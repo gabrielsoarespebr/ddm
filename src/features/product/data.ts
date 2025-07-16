acb86ad7b25efe722646fe8a32e5aca2d36d2b99
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2025-07-16 15:48:12 -0300

    feat: Mock product list data

--- src/features/product/data.ts
+++ src/features/product/data.ts
@@ -1,5 +1,5 @@
 export interface Product {
-  id: number;
+  id: string;
   supplierCode: string;
   name: string;
   description: string;
@@ -9,39 +9,259 @@ export interface Product {
 
 export const productList: Product[] = [
   {
-    id: 1,
+    id: "CI0000",
     supplierCode: "ROY",
-    name: "CORREIA IND. REXON BORRACHA 1890 S14M 85mm",
-    description: `Correia e polia sincronizadora de passo 14M (para acionamento extra pesado Ex.: máquina-ferramenta pesada, trituradores, etc.)`,
-    imageUrl:
-      "https://s3-sa-east-1.amazonaws.com/loja2/aaf0f616deee9955bd844bc8a282dbff.jpg",
-    category: "correias-industriais",
-  },
-  {
-    id: 2,
-    supplierCode: "ROY",
-    name: "Correia 14M 1190 55mm de Largura (1190 14M) Sincronizadora Optibelt Omega",
-    description: `Informações técnicas:
-Correia Sincronizadora 14M 1190
-Largura: 55mm
-Numero de dentes: 85
-Correia 1190 14M HTD
-Perfil do dente: 14M (14.00mm)
-Consulte outras medidas.
-Correia sincronizadora de passo 14M (para acionamento extra pesado Ex.: máquina-ferramenta pesada, trituradores, etc.).
+    name: "Correia 14M 1190 40mm de Largura (1190 14M) Sincronizadora Optibelt Omega",
+    description: `
+Material Construtivo: Borracha Neopre
+Aplicação: Industrial
+Largura: 40mm
+Marca: Optibelt
+Perfil: 14M
+Referência: 14M 1190 40
+Modelo: Omega Htd
+Tipo: Sincronizadora
+Quantidade de dentes: 85
+Comprimento em MM: 1190
+
+Atenção: Geometria dos dentes não é compatível com Perfil STD®
+
+Obs.: Outras larguras, comprimentos e número de dentes mediante consulta.
+Circunferência: Medir a correia como se estivesse medindo a "cintura" de uma pessoa.
+Temperatura no local da transmissão de -30º até 70ºC
+Obs: Por favor, consultar disponibilidade.
 `,
     imageUrl:
-      "https://s3-sa-east-1.amazonaws.com/loja2/063c33206f6d23a37adfd61ef7e52c9b.jpg",
+      "https://s3-sa-east-1.amazonaws.com/loja2/29def4e3546892992f8fcf234053aad0.jpg",
     category: "correias-industriais",
   },
   {
-    id: 3,
+    id: "CI0001",
     supplierCode: "ROY",
-    name: "Carregador de Bateria CBF12-50B",
-    description: `Para carregar baterias até 150 amperes e auxilia a
-partida em carros de passeio e utilitários.`,
+    name: "Correia Polyflex JB Pu - 7M1030 Gates (7M1030) Poliuretano",
+    description: `
+Material Construtivo: Poliuretano
+Aplicação:	Industrial
+Largura:	7mm
+Marca:	Gates
+Perfil: V
+Referência:	7M1030
+Modelo:	Polyflex
+Comprimento em MM: 1030
+
+Descrição:
+A Gates é referência mundial no fornecimento de correias industriais.
+Com uma ampla linha de correias industriais, atendemos todas as necessidades com produtos exclusivos, conferindo durabilidade e economia aos nossos clientes.
+
+CORREIA POLYFLEX ® JB (3M, 5M, 7M e 11M)
+
+Proporcionam maior estabilidade das correias, prevenindo que a correia vire ou se desloque para fora da transmissão. Em uma angulação de 60º, feita de poliuretano, sua construção única transfere cargas maiores a velocidades elevadas em menor espaço do que correias em “V” convencionais. Funcionamento uniforme e suave com baixa vibração.
+
+Mercado e Aplicação:
+
+Usada em lugar da correias Polyflex simples em transmissões que precisem de jogos unidos. Uma lista parcial das aplicações inclui máquinas-ferramentas, peças periféricas de computadores, equipamentos médicos, equipamentos de bibliotecas, eletrodomésticos de pequeno porte, secadores e misturadores de massa.
+
+Disponíveis nos perfis 3M, 5M, 7M e 11M.
+
+Características e Vantagens das Correias Polylfex:
+
+O Composto de Poliuretano de alta rigidez resiste à fadiga, desgaste, ozônio e às demais condições ambientais.
+Método preciso de fundição elimina camadas e sobreposições de modo a proporcionar um funcionamento uniforme e suave com baixa vibração.
+Dorso frisado proporciona maior estabilidade lateral e alivia o esforço de flexão.
+Gama de temperatura de trabalho: -54°C até 85°C (-65°F até +185°F).
+Construção de banda unida proporciona alta estabilidade, prevenindo que a correia vire ou se desloque para fora da transmissão.
+
+Polias Recomendadas: Polias PolyFlex
+`,
     imageUrl:
-      "https://s3-sa-east-1.amazonaws.com/loja2/fda1a7949b9d40b5a3d7a1648c5c0081.jpg",
+      "https://s3-sa-east-1.amazonaws.com/loja2/fd626f6b669080c32cd6af9f971ebca9.jpg",
+    category: "correias-industriais",
+  },
+  {
+    id: "ME0000",
+    supplierCode: "ROY",
+    name: "Furadeira Industrial FC-45",
+    description: `
+Marca: Ferrari
+Modelo: FC-45
+Tipo: Furadeira Indus
+Potência:	2 cv (1500 Watt
+Tensão nominal:	127/220 Volts
+
+APLICAÇÃO:
+
+Ideal para operações em produção industrial e execução em grande escala.
+
+BENEFÍCIOS:
+
+Possui mesa giratória (360°) e inclinável até 90°.
+Sistema para retirada da broca (pinça) cone morse n° 3
+Cabeçote inclinável até 90°.
+Avanço automático do eixo árvore.
+Sistema de iluminação - Proporciona maior visualização na furação.
+Cabeçote com caixa de engrenagem.
+Sistema de lubrificação automático com bomba de óleo que proporciona
+maior precisão na furação.
+Robustez
+
+CARACTERÍSTICAS TÉCNICAS:
+
+Motor:2 cv (1500 Watts)
+Tensão Nominal:127/220 Volts
+Capacidade máxima de furação Ø:Ø 45 mm
+Cone Morse:N° 4
+Avanço máximo do eixo árvore:130 mm
+Gama de Velocidade:de 95 a 1500 rpm
+Distância entre a coluna e a árvore:283 mm
+Distância máxima entre a árvore e a mesa:730 mm
+Distância máxima entre a árvore e a base:1130 mm
+Curso máximo do cabeçote:250 mm
+Número de velocidades do avanço da árvore:6 velocidades
+Velocidades do avanço da árvore:de 0.06 a 0.30
+Avanço automático do eixo árvore:sim
+Inclinação do cabeçote perpendicular: 90°
+Curso máximo da mesa:600 mm
+Área útil da mesa:380 x 300 mm
+Área útil da base:417 x 416 mm
+Inclinação da mesa: 90°
+Giro da mesa:360°
+Dimensões do produto:850 x 700 x 1960 mm
+Peso do produto:430 Kg
+`,
+    imageUrl:
+      "https://s3-sa-east-1.amazonaws.com/loja2/551d1c31bf876c3d69b2256c81dfd90c.jpg",
     category: "maquinas-e-equipamentos",
+  },
+  {
+    id: "ME0001",
+    supplierCode: "ROY",
+    name: "Lixadeira combinada LCM-46",
+    description: `
+Marca:	Ferrari
+Modelo:	LCM-46
+Tipo:	Lixadeira
+Potência:	½ cv (370 Watts
+Tensão nominal:	127 / 220 Volts
+
+APLICAÇÃO:
+
+Indicada para trabalhos leves em artesanatos, marcenarias, etc.
+Para lixar e dar acabamento em madeiras.
+
+BENEFÍCIOS:
+
+Mesa Lateral ajustável até 45º.
+2 Posições de lixamento - Horizontal e Vertical.
+Chave seletora de voltagem 127V / 220V bivolt - Permite ao usuário mudar a
+voltagem com facilidade, praticidade e rapidez.
+
+CARACTERÍSTICAS TÉCNICAS:
+
+Motor:½ cv (370 Watts)
+Rotação do motor:1750 rpm
+Tensão Nominal:127 / 220 Volts
+Capacidade máxima de lixamento do disco:Ø 150 mm
+Capacidade máxima de lixamento da cinta:300 x 100 mm
+Tamanho da lixa de disco:6” (150 mm)
+Tamanho da lixa de cinta:4" x 36" (100 x 920 mm)
+Dimensões da mesa lateral:190 x 125 mm
+Dimensões do produto:500 x 400 x 340 mm
+Dimensões da embalagem:500 x 260 x 300 mm
+Peso:13.5 kg
+`,
+    imageUrl:
+      "https://s3-sa-east-1.amazonaws.com/loja2/5375c458f4e6ecc7b39e2af75bb3fa91.jpg",
+    category: "maquinas-e-equipamentos",
+  },
+  {
+    id: "PO0000",
+    supplierCode: "ROY",
+    name: "Polia 5M 48 Dentes 25mm - Alumínio",
+    description: `
+Material Construtivo:	Aço
+Aplicação: Industrial
+Marca: Optibelt
+Perfil:	5M
+Referência: 485M25
+Modelo:	6W
+Tipo:	Sincronizadora
+Embalagem/Quant.:	1
+Número De Dentes:	48
+`,
+    imageUrl:
+      "https://s3-sa-east-1.amazonaws.com/loja2/58845a86debfd79b4bda1da325bdfd2f.png",
+    category: "polias",
+  },
+  {
+    id: "RO0000",
+    supplierCode: "ROY",
+    name: "Rolamentos Cooper",
+    description: `
+Rolamentos COOPER
+Fornecemos toda a linha de rolamentos COOPER bi-partidos, rolamentos de rolos cilíndricos, rolamentos de rolos cônicos, e rolamentos especiais, como por exemplo: rolamentos de dupla carreira, sem gaiola, rolamentos em mancais refrigeradas a água e outros modelos especiais sob encomenda.
+
+Os Rolamentos COOPER são ideais para serviços pesados ou leves, bem como de tamanhos compactos e para altas velocidades, tanto para cargas radiais ou axiais.
+
+Os rolamentos COOPER estão disponíveis tanto como modelo fixo (bloqueado) ou livre axialmente, além de estarem disponíveis com furos em milímetros ou polegadas.
+
+Industrias e aplicações
+Rolamentos COOPER são encontrados em todas as indústrias, onde são submetidos a duras condições de trabalho, incluindo:
+
+Siderúrgicas
+Mineração
+Naval
+Geração de energia
+Cimento e outros materiais a granel
+Açúcar
+Os rolamentos COOPER também são utilizados em aplicações onde confiabilidade e flexibilidade são vitais para a elevada rentabilidade da empresa, incluindo:
+
+Transportadores
+Sistemas de propulsão naval
+Ventiladores
+Leito de resfriamento
+Empilhadeiras
+Motores e geradores
+`,
+    imageUrl:
+      "https://s3-sa-east-1.amazonaws.com/loja2/404a5ce1ec1faae55c07f450410b6980.jpg",
+    category: "rolamentos-industriais",
+  },
+  {
+    id: "RO0001",
+    supplierCode: "ROY",
+    name: "Rolamentos Link Belt",
+    description: `
+Rolamentos radiais de esferas e de agulhas ;
+Rolamentos axiais de rolos, de esferas e de agulhas ;
+Rolamentos combinados agulha / esfera ;
+Rolamentos de rolos cilíndricos ;
+Rolamentos de rolos cônicos ( CAPA E CONE ) ;
+Rolamentos de contato angular de Super Precisão ;
+Rolamentos de contato angular de Dupla Carreira de esferas ;
+Rolamento auto-compensadores de esferas, de rolos ;
+Rolamentos com eixos sextavados e
+Rolamentos especiais conforme desenho.
+
+Estes rolamentos constam com os mais diversos tipos de medidas e materiais que servem a todos os segmentos da indústria.
+`,
+    imageUrl:
+      "https://s3-sa-east-1.amazonaws.com/loja2/c0e64915775ca7b0d601bcd5b952a355.jpg",
+    category: "rolamentos-industriais",
+  },
+  {
+    id: "RO0002",
+    supplierCode: "ROY",
+    name: "Rolamentos Nice",
+    description: `
+Nice Ball Bearing foi criado em 1902, quando William Nice iniciou uma das primeiras instalações do mundo para a fabricação de rolamentos.
+No mercado de hoje, a Nice oferece rolamentos e montagens não aterrados, semi-terrestres e de precisão.
+Os rolamentos de esferas são oferecidos em várias combinações de selos e tamanhos.
+Os encaixes não acoplados são perfeitos para o cliente que não requerem rolamento adequado para altas velocidades.
+
+Os rolamentos de esferas Nice Ball Bearing agora são oferecidos através do RBC Bearings e estão disponíveis para atender um mercado mundial. Várias indústrias utilizam os rolamentos de Nice em suas instalações, incluindo: têxteis, automotivo, agricultura e manuseio de materiais.
+`,
+    imageUrl:
+      "https://s3-sa-east-1.amazonaws.com/loja2/5ef57efd0986567f4783404155eb93cd.jpg",
+    category: "rolamentos-industriais",
   },
 ];
