12a907971af7161ad125266df7734b2462332a8f
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2025-09-01 23:40:55 -0300

    refactor: CompanyMissionCard - Animation

--- src/pages/Home/components/CompanyMissionCard/CompanyMissionCard.tsx
+++ src/pages/Home/components/CompanyMissionCard/CompanyMissionCard.tsx
@@ -1,8 +1,16 @@
 import "./style.css";
+
+import { motion, useInView } from "motion/react";
+import { useRef } from "react";
+
 import type { MissionProps } from "../../../../types/MissionProps";
 import { Eye, Gem, Target } from "lucide-react";
 
 export const CompanyMissionCard = ({ mission }: MissionProps) => {
+  // Framer motion
+  const companyMissionCard = useRef(null);
+  const isInView = useInView(companyMissionCard, { once: true });
+
   return (
     <div
       className={
@@ -11,7 +19,17 @@ export const CompanyMissionCard = ({ mission }: MissionProps) => {
         ""
       }
     >
-      <div className="mission-card">
+      <motion.div
+        ref={companyMissionCard}
+        variants={{
+          hidden: { opacity: 0, y: 50 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate={isInView ? "visible" : "hidden"}
+        transition={{ duration: 2 }}
+        className="mission-card"
+      >
         <div>
           <div className="mission-title flex items-center gap-3">
             {(mission.title === "Visão" && <Eye />) ||
@@ -22,7 +40,7 @@ export const CompanyMissionCard = ({ mission }: MissionProps) => {
           <p className="mission-description">{mission.description}</p>
         </div>
         <img src={mission.imageUrl} alt={mission.textAlt} loading="lazy" />
-      </div>
+      </motion.div>
     </div>
   );
 };
