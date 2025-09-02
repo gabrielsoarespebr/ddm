1d13d9ec4c55883421205dbca6272fa2566a5803
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2025-09-01 22:57:07 -0300

    feat: WhatsApp icon

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,23 @@ import "./App.css";
 import { Home } from "./pages/Home/Home";
 import { store } from "./app/store";
 import { Header } from "./shared/components/Header/Header";
+import whatsappIcon from "./assets/images/whatsappIcon.webp";
 
 function App() {
   return (
     <Provider store={store}>
       <Header />
       <Home />
+      <a
+        href="https://wa.me/558183022802"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Entrar em contato pelo WhatsApp"
+        aria-label="Entrar em contato pelo WhatsApp"
+        id="whatsapp-fixed-icon"
+      >
+        <img src={whatsappIcon} alt="Ícone de WhatsApp" />
+      </a>
     </Provider>
   );
 }
