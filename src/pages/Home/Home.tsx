40c9c98c015928245f349e9fc2bc8354a6252ead
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2025-09-07 22:34:48 -0300

    responsive layout (small and medium devices): Home - Banner - Horizontal padding and text alignment

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,9 +7,9 @@ export const Home = () => {
   return (
     <div>
       <div id="banner">
-        <div className="flex flex-col justify-center items-center">
-          <p className="highlight-title-text">Soluções industriais eficazes</p>
-          <p className="my-7">
+        <div className="flex flex-col justify-center items-center px-8 lg:px-0">
+          <p className="highlight-title-text text-center">Soluções industriais eficazes</p>
+          <p className="my-7 text-center">
             Peças, serviços e suporte técnico que aumentam a performance da sua
             operação.
           </p>
