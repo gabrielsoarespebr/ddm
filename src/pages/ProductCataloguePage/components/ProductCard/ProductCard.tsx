4917d4899aebc79c35a65761b67bafa88bbcf48c
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2025-09-30 23:56:51 -0300

    style: ProductCard - addToCart button background color and hover animation

--- src/pages/ProductCataloguePage/components/ProductCard/ProductCard.tsx
+++ src/pages/ProductCataloguePage/components/ProductCard/ProductCard.tsx
@@ -1,3 +1,4 @@
+import "./style.css";
 import { useAppDispatch } from "../../../../app/hooks";
 import { addToCart } from "../../../../features/cart/cartSlice";
 import type { ProductProps } from "../../../../types/productProps";
@@ -20,7 +21,8 @@ export const ProductCard = ({ product }: ProductProps) => {
           <p className="font-bold">{product.name}</p>
         </div>
         <button
-          className="w-full p-2 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded-b cursor-pointer"
+          className="w-full p-2 text-white font-bold rounded-b cursor-pointer"
+          id="id-to-cart-btn"
           onClick={() => dispatch(addToCart(product))}
         >
           Adicionar ao orçamento
