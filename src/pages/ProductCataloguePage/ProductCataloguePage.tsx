4a30609d4daa2b07d1eb977328fab6e974092a8d
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2025-11-28 01:17:03 -0300

    feat: ProductCataloguePage - Cart

--- src/pages/ProductCataloguePage/ProductCataloguePage.tsx
+++ src/pages/ProductCataloguePage/ProductCataloguePage.tsx
@@ -1,12 +1,14 @@
 import "./style.css";
 import { ProductCatalogue } from "./components/ProductCatalogue/ProductCatalogue";
 import { CatalogueFilterPanel } from "./components/CatalogueFilterPanel/CatalogueFilterPanel";
+import { Cart } from "./components/Cart/Cart";
 
 export const ProductCataloguePage = () => {
   return (
     <div id="catalogue">
       <CatalogueFilterPanel />
       <ProductCatalogue />
+      <Cart />
     </div>
   );
 };
